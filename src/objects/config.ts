94e2f6eedfd0d6a5ff4702700a55c76d2171ab73
Youenn Gasc <youenngasc@gmail.com>
2025-10-11 01:06:32 +0200

    update config

--- src/objects/config.ts
+++ src/objects/config.ts
@@ -1,5 +1,5 @@
 const isProd = window.location.hostname !== "localhost";
 
 export const backendURL: string = isProd
-  ? "https://onecityperday-backend-976609dd802a.herokuapp.com/cities"
+  ? "https://api.onecityperday.com/cities"
   : "http://localhost:8080/cities";
